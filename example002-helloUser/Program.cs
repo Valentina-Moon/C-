346c88693cfaed2635864659c135ca0d7636ffe0
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-02 14:32:55 +0300

    hellouser

---
+++ example002-helloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// начало- ввод username -ввод "ПРИВЕТ," - вывод username -конец
+//= c права положи в лево
+Console.WriteLine("введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);

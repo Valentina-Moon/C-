a9f8c955b1a067597a4f9c07b4e3011fd047535e
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-02 15:33:27 +0300

    рандом чисел

---
+++ example003_Random/Program.cs
@@ -0,0 +1,8 @@
+﻿// генератор псевдослучайных чисел new Random().Next(min,max)
+// даст случайное целое число от min до  max-1 [min, max] или [min, max)
+int numberA = new Random().Next(1,10) ;// 0 1 2...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);

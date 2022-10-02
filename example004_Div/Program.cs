a1bf389a7dad8104fb8f0e0ca6ba5293cb959b5c
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-02 14:58:52 +0300

    деление

--- example004_Div/Program.cs
+++ example004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// частное двух чисел// деление с остатком
+int numberA = 10;// double numberA = 12
+int numberB = 5;// double numberB = 5
+Console.WriteLine(numberA/numberB);

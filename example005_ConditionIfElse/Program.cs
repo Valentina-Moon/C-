991d4fe340c7fc4919ad06cd387de9bec80d0c4b
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-02 16:13:22 +0300

    IF ELSE

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,22 @@
+﻿// if (условия)
+//{
+    // набор действий 1
+    //}
+//{
+    //else
+//}
+//{
+    //набор действий 2
+//}
+Console.Write("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
